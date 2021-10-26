24268686018925b46c62cba4aea38ff60e5ca9d2
park3987 <91654027+park3987@users.noreply.github.com>
2021-10-26 18:00:10 +0900

    .

--- gameplay.py
+++ gameplay.py
@@ -1,106 +1,178 @@
 import pygame
 import random
+from pygame.locals import *
 
-pygame.init()  # 초기화 (반드시 필요)
+pygame.init()
+width, height = 640, 480
+screen = pygame.display.set_mode((width, height))
+keys = [False, False, False, False]
+charecterpos = [10, 10]
+apples = [[50, 50]]
+applecollecters = [[]]
+appletimer = 30
+appletimer1 = 0
+totalapple = 0
+villons = [[50, 50]]
+villoncollecters = [[]]
+villontimer = 1
+villontimer1 = 0
+game_over = False
 
-# 화면 크기 설정
-screen_width = 1250  # 가로 크기
-screen_height = 755  # 세로 크기
-screen = pygame.display.set_mode((screen_width, screen_height))
-apple_width = 30
-apple_height = 30
-# 점수 설정
-score = 0
+backGround = pygame.image.load("src/background.png")
+backgroundimg = pygame.transform.scale(backGround, (640, 480))
+charecterimg = pygame.image.load("src/caa.png")
+appleimg = pygame.image.load("src/appleee.png")
+gameoverimg = pygame.image.load("src/end.png")
+villonimg = pygame.image.load("src/vii.png")
 
-pygame.display.set_caption("Game")  # 게임 이름
-
-# 배경 이미지 불러오기
-
-background = pygame.image.load("C:/Users/bluew/PycharmProjects/pythonProject2/src/background.png")
-
-apple = pygame.image.load("C:/Users/bluew/PycharmProjects/pythonProject2/src/appleee.png")
-apple_x_pos = random.randint(0, 1250)
-apple_y_pos = 0
-
-villon = pygame.image.load("C:/Users/bluew/PycharmProjects/pythonProject2/src/vii.png")
-villon_size = villon.get_rect().size
-villon_width = villon_size[0]
-villon_height = villon_size[1]
-villon_x_pos = random.randint(0, 1250)
-villon_y_pos = 0
-
-# 캐릭터(스프라이트) 불러오기
-character = pygame.image.load("C:/Users/bluew/PycharmProjects/pythonProject2/src/caa.png")
-character_size = character.get_rect().size  # 이미지의 크기를 구해옴
-character_width = character_size[0]  # 캐릭터의 가로 크기
-character_height = character_size[1]  # 캐릭터의 세로 크기
-character_x_pos = (screen_width / 2) - (character_width / 2)  # 화면 가로의 절반 크기에 해당하는 곳에 위치 (가로)
-character_y_pos = screen_height - character_height  # 화면 세로 크기 가장 아래에 해당하는 곳에 위치 (세로)
-
-# 이동할 좌표
-to_x = 0
-to_y = 0
-
-vil_x = 0
-vil_y = 0
-
-app_x = 0
-app_y = 0
-
-running = True
+running = 1
+exitcode = 0
 while running:
-    for event in pygame.event.get():  # 어떤 이벤트가 발생하였는가?
-        if event.type == pygame.QUIT:  # 창이 닫히는 이벤트가 발생하였는가?
-            running = False  # 게임이 진행중이 아님
+    appletimer -= 1
+    villontimer -= 1
+    screen.fill(0)
+    screen.blit(backgroundimg, (0, 0))
+    screen.blit(charecterimg, charecterpos)
+    if appletimer == 0:
+        apples.append([640, random.randint(20, 470)])
+        appletimer = 100 - (appletimer1*2)
+        if appletimer1 >= 35:
+            appletimer1 = 35
+        else:
+            appletimer1 += 5
+    index = 0
+    for apple in apples:
+        if apple[0] <- 64:
+            apples.pop(index)
+        apple[0] -= 1
+        AppleRect = pygame.Rect(appleimg.get_rect())
+        AppleRect.top = apple[1]
+        AppleRect.left = apple[0]
 
+        index1 = 0
+        for applecollecter in applecollecters:
+            charecterrect = pygame.Rect(charecterimg.get_rect())
+            charecterrect.top = charecterpos[1]
+            charecterrect.left = charecterpos[0]
+            if charecterrect.colliderect(AppleRect):
+                totalapple += 1
+                apples.pop(index)
+            index1 += 1
+
+        index += 1
+    for apple in apples:
+        screen.blit(appleimg, apple)
+
+    if villontimer == 0:
+        villons.append([640, random.randint(20, 470)])
+        villontimer = 200 - (villontimer1 % 10)
+        if villontimer1 >= 10:
+            villontimer1 = 10
+        else:
+            villontimer1 += 5
+
+    index = 0
+    for villon in villons:
+        if villon[0] <- 64:
+            villons.pop(index)
+        villon[0] -= 1
+        VillonRect = pygame.Rect(villonimg.get_rect())
+        VillonRect.top = villon[1]
+        VillonRect.left = villon[0]
+
+        index1 = 0
+        for villoncollecter in villoncollecters:
+            charecterrect = pygame.Rect(charecterimg.get_rect())
+            charecterrect.top = charecterpos[1]
+            charecterrect.left = charecterpos[0]
+            if charecterrect.colliderect(VillonRect):
+                totalapple -= 1
+                villons.pop(index)
+            index1 += 1
+        index += 1
+    for villon in villons:
+        screen.blit(villonimg, villon)
+
+    font = pygame.font.SysFont('malgungothic', 18)
+    survivedtext = font.render('%d'%totalapple+"점", True, (0, 0, 0))
+    textRect = survivedtext.get_rect()
+    textRect.topright = [635, 5]
+    screen.blit(survivedtext, textRect)
+
+    pygame.display.flip()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit(0)
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:  # 캐릭터를 왼쪽으로
-                to_x -= 0.5  # to_x = to_x - 5
-            elif event.key == pygame.K_RIGHT:  # 캐릭터를 오른쪽으로
-                to_x += 0.5
-            elif event.key == pygame.K_UP:  # 캐릭터를 위로
-                to_y -= 0.5
-            elif event.key == pygame.K_DOWN:  # 캐릭터를 아래로
-                to_y += 0.5
+            if event.key == K_UP:
+                keys[0] = True
+            elif event.key == K_LEFT:
+                keys[1] = True
+            elif event.key == K_DOWN:
+                keys[2] = True
+            elif event.key == K_RIGHT:
+                keys[3] = True
+        if event.type == pygame.KEYUP:
+            if event.key == K_UP:
+                keys[0] = False
+            elif event.key == K_LEFT:
+                keys[1] = False
+            elif event.key == K_DOWN:
+                keys[2] = False
+            elif event.key == K_RIGHT:
+                keys[3] = False
+    if keys[0]:
+        charecterpos[1] -= 1
+    if keys[1]:
+        charecterpos[0] -= 1
+    if keys[2]:
+        charecterpos[1] += 1
+    if keys[3]:
+        charecterpos[0] += 1
 
-        vil_y += random.choice([0.01, 0.009, 0.025786778786867675657689897846778736457, 0.03, 0.03232323])
-        app_y += random.choice([0.01, 0.009, 0.025786778786867675657689897846778736457, 0.03, 0.03232323])
+    if pygame.time.get_ticks() >= 13000:
+        running = 0
+        exitcode = 1
 
-        if event.type == pygame.KEYUP:  # 방향키를 떼면 멈춤
-            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                to_x = 0
-            elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-                to_y = 0
+    if charecterpos[0] >= 50:
+        charecterpos[0] = 50
 
-    character_x_pos += to_x
-    character_y_pos += to_y
+    if charecterpos[0] <= 0:
+        charecterpos[0] = 0
 
-    apple_x_pos += app_x
-    apple_y_pos += app_y
+    if charecterpos[1] <= 0:
+        charecterpos[1] = 0
 
-    villon_y_pos += vil_y
+    if charecterpos[1] >= 450:
+        charecterpos[1] = 450
 
-    # 가로 경계값 처리
-    if character_x_pos < 0:
-        character_x_pos = 0
-    elif character_x_pos > screen_width - character_width:
-        character_x_pos = screen_width - character_width
+if exitcode == 1:
+    pygame.font.init()
+    font = pygame.font.SysFont('malgungothic', 50)
+    survivedtext2 = font.render('%d'%totalapple+"점", True, (0, 0, 0))
+    textRect = survivedtext2.get_rect()
+    textRect.centerx = screen.get_rect().centerx
+    textRect.centery = screen.get_rect().centery - 40
+    screen.blit(gameoverimg, (0, 0))
+    screen.blit(survivedtext2, textRect)
 
-    # 세로 경계값 처리
-    if character_y_pos < 700:
-        character_y_pos = 700
-    elif character_y_pos > screen_height - character_height:
-        character_y_pos = screen_height - character_height
+    if totalapple > 50:
+        survivedtext3 = font.render("이게 가능한건가..?", True, (0, 0, 0))
+        texRect = survivedtext3.get_rect()
+        texRect.centerx = screen.get_rect().centerx
+        texRect.centery = screen.get_rect().centery + 50
+        screen.blit(survivedtext3, texRect)
+    if totalapple < 20:
+        survivedtext4 = font.render("뭐하세요?", True, (0, 0, 0))
+        texttRect = survivedtext4.get_rect()
+        texttRect.centerx = screen.get_rect().centerx
+        texttRect.centery = screen.get_rect().centery + 50
+        screen.blit(survivedtext4, texttRect)
 
-    screen.blit(background, (0, 0))
-
-    screen.blit(character, (character_x_pos, character_y_pos))  # 캐릭터 그리기
-
-    screen.blit(apple, (apple_x_pos, apple_y_pos))
-
-    screen.blit(villon, (villon_x_pos, villon_y_pos))
-
-    pygame.display.update()
-
-# pygame 종료
-pygame.quit()
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit(0)
+    pygame.display.flip()
